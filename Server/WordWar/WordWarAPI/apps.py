e9027f137be14465afea453fcfcdcab313c0501b
KooHyongMo <christopher@guhyeongmoui-MacBook-Pro.local>
2019-05-28 16:03:35 +0900

    setting done

---
+++ Server/WordWar/WordWarAPI/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WordwarapiConfig(AppConfig):
+    name = 'WordWarAPI'
---
+++ Server/WordWar/WordWarAPI/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UserForm, LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+            return redirect('index')
+        else:
+            return HttpResponse('사용자명이 이미 존재합니다.')
+    else:
+        form = UserForm()
+        return render(request, 'auth/adduser.html', {'form': form})
+
+def signin(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponse('로그인 실패. 다시 시도 해보세요.')
+    else:
+        form = LoginForm()
+        return render(request, 'auth/login.html', {'form': form})
+
+def index(request):
+    return render(request, 'default.html')
+
