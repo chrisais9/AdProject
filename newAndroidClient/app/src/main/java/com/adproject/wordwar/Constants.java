6d6730b7bb65f178bddc749bd7c002a29ef0b98c
KooHyongMo <christopher@guhyeongmoui-MacBook-Pro.local>
2019-06-18 23:45:19 +0900

    .

--- newAndroidClient/app/src/main/java/com/adproject/wordwar/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.adproject.wordwar;
-
-public class Constants {
-    public static final String CHAT_SERVER_URL = "http://10.30.98.3:3000";
-}
---
+++ socket.io-android-chat-master/app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -0,0 +1,5 @@
+package com.github.nkzawa.socketio.androidchat;
+
+public class Constants {
+    public static final String CHAT_SERVER_URL = "http://10.30.98.3:3000";
+}
